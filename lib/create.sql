86ca55c6d94f017eece9cb409e0377c749ef41ab
enock-jufri <enockjufri@gmail.com>
2025-01-19 10:05:44 +0300

    sql queries

---
+++ lib/create.sql
@@ -0,0 +1,2 @@
+CREATE TABLE bears 
+( id integer PRIMARY KEY, name text, age integer, sex text, color text, temperament text, alive boolean );
---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES 
+("Mr Chocolate",10,"F","brown","playful",1),
+("Rowdy",12,"M","brown","docile",0),
+("Tabitha",8,"F","white","loyal",1), 
+("Sergeant Brown",15,"F","brown","protective",0), 
+("Melissa",5,"M","black","gentle",0), 
+("Grinch",7,"F","brown","aggressive",1), 
+("Wendy",9,"M","white","playful",0), 
+(NULL,13,"M","Black","gentle",1);
